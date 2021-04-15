342b30eb61526e1c6428a04787648827769410ae
Xabi <xabi.aycaguer@gmail.com>
2021-04-15 14:08:52 +0200

    journalists V1

---
+++ lib/00_journalists.rb
@@ -0,0 +1,104 @@
+# Tableux des handles
+def journalist
+handle =["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@min","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@ceci_est_un_handle_vraiment_long","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+end
+
+# Méthode pour le calcul total
+def total(handle)
+  puts "Il y a #{handle.count} journalistes véreux dans ce fichier."
+  perform
+end
+
+# Méthode pour le tri dans l'odre alphabétique
+def tri_a_z(handle)
+  handle_a_z = handle.sort_by {|element| element.size}
+end
+
+# Méthode pour le tri croissant par nombre de caractères et afficher le 1er
+def min_handle(handle_1_2)
+  puts "Le journaliste au nom le plus court est #{handle_1_2.first} (il a surement la plus courte également...)"
+  perform
+end
+
+# Méthode pour afficher les handles de 5 caractères
+def tri_5_caracter(handle)
+  puts "Il y a #{handle.count {|element| element.size==6}} noms avec 5 caractères. Why not?"
+  perform
+end
+
+# Méthode pour afficher le nombre de handles commencant par une maj
+def tri_majuscule(handle)
+  handle_withoutA = handle.each {|element| element.slice!(0)}
+  puts "Il y a #{handle_withoutA.count{|element| element[0]== element[0].capitalize}} noms qui commencent par une majuscule"
+  perform
+end
+
+# Méthode pour trier par ordre alphabétique
+def handle_a_z (handle)
+  puts "Voici l'ordre alphabétique des journalistes:"
+  puts handle.sort_by(&:downcase)
+  perform
+end
+
+# Méthode pour le tri croissant par nombre de caractères
+def handle_by_size(handle_1_2)
+  puts "Voici les noms par ordre de taille:"
+  puts handle_1_2
+  perform
+end 
+
+# Méthode pour afficher l'index d'un handle
+def where_is_he (handle)
+  puts "@epenser est à la position #{handle.index("@epenser"+1)} du notre super tableau"
+end
+
+# Méthode pour répartir les handles par nombre de caractères
+def repartition_size(handle)
+  puts "Les handles de quel nombre de caractère souhaites-tu afficher?"
+  print "> "
+  number=gets.chomp.to_i
+  puts "Voici la liste de handles à #{number} caractères"
+  puts total= handle.select {|element| element.size==number+1 }
+  puts "Il y en a au total #{total.count}"
+  perform
+end
+
+# Méthode menu + lancer vers les méthodes
+def perform
+  handle = journalist
+  handle_1_2 =tri_a_z(handle)
+  puts "___________________________________________________________________"
+  puts "                 Bonjour, que voulez vous savoir ?                 "
+  puts ""
+  puts "|> Le nombre de handle dans la liste :               \033[1;32m> Tapes 1 <"+"\033[0m"
+  puts "|> Le handle le plus court :                         \033[1;32m> Tapes 2 <"+"\033[0m"
+  puts "|> Combien il y a de handle contenant 5 caractères : \033[1;32m> Tapes 3 <"+"\033[0m"
+  puts "|> Combien de handle commencent par une majuscule :  \033[1;32m> Tapes 4 <"+"\033[0m"
+  puts "|> La liste des handles dans l'ordre alphabétique :  \033[1;32m> Tapes 5 <"+"\033[0m"
+  puts "|> Trier les handles par taille :                    \033[1;32m> Tapes 6 <"+"\033[0m"
+  puts "|> La position de @epenser dans la liste :           \033[1;32m> Tapes 7 <"+"\033[0m"
+  puts "|> Une répartition des handles par tailles :         \033[1;32m> Tapes 8 <"+"\033[0m"
+  puts "|> Pour quitter                                      \033[1;41m> Tapes 0 <"+"\033[0m"
+  puts "___________________________________________________________________"
+  print ">>>> "
+  user_choose=gets.chomp.to_i
+  if user_choose==1
+    total(handle)
+    elsif user_choose==2
+      min_handle(handle_1_2)
+    elsif user_choose==3
+      tri_5_caracter(handle)
+    elsif user_choose==4
+      tri_majuscule(handle)
+    elsif user_choose==5
+      handle_a_z(handle)
+    elsif user_choose==6
+      handle_by_size(handle_1_2)
+    elsif user_choose==7
+      where_is_he(handle)
+    elsif user_choose==8
+      repartition_size(handle)  
+  end
+end
+
+perform
